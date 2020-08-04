263ed6f29db89e6954c39c765126e049ae6b4832
nikolatriki <kjofte@yahoo.com>
2020-08-04 23:45:43 +0200

    Completed all Replies controller actions

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -5,10 +5,37 @@ class RepliesController < ApplicationController
   end
 
   def create
-    tweet = Tweet.find(params[:tweet_id])
-    reply = tweet.replies.build(reply_params)
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = @tweet.replies.build(reply_params)
 
-    reply.save
+    if @reply.save
+      redirect_to @tweet
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @reply = Reply.find(params[:id])
+    @tweet = @reply.tweet
+  end
+
+  def update
+    @reply = Reply.find(params[:id])
+    @tweet = @reply.tweet
+
+    if @reply.update(reply_params)
+      redirect_to @tweet
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    reply = Reply.find(params[:id])
+    tweet = reply.tweet
+
+    reply.destroy
     redirect_to tweet
   end
 
