90873dccac83839b337f7ed980fd9d68b446b9a7
nikolatriki <kjofte@yahoo.com>
2020-08-05 16:44:52 +0200

    Create User model with validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_144528) do
+ActiveRecord::Schema.define(version: 2020_08_05_143324) do
 
   create_table "replies", force: :cascade do |t|
     t.string "replier"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2020_07_30_144528) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "replies", "tweets"
