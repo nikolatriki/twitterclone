5676269d8f5b7d7abb974b634145b362813155c5
nikolatriki <kjofte@yahoo.com>
2020-08-06 23:14:34 +0200

    Created Users controller, views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root to:'tweets#index'
+  root to: 'tweets#index'
   resources :tweets do
     resources :replies
   end
+  get 'users/signup', to: 'users#new'
+  resources :users, except: :new
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
